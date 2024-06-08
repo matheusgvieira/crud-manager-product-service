7644a9a5054d574e78d11d77df7e28f3595ffe46
Matheus Gois Vieira <matheusmaxi9.0@gmail.com>
2024-06-08 11:15:07 -0300

    test(app): add tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,27 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { MongooseModule } from "@nestjs/mongoose";
 import { mongo_config } from "@config/mongo.config";
+import { MongoMemoryServer } from "mongodb-memory-server";
+import Environment from "@shared/utils/environment.util";
 
 @Module({
-  imports: [TerminusModule, MongooseModule.forRoot(mongo_config.url)],
+  imports: [
+    TerminusModule,
+    MongooseModule.forRootAsync({
+      useFactory: async () => {
+        if (Environment.is_test) {
+          const mongod = await MongoMemoryServer.create();
+          const uri = mongod.getUri();
+          return {
+            uri,
+          };
+        }
+        return {
+          uri: mongo_config.url,
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,25 +1,31 @@
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import { INestApplication } from "@nestjs/common";
+import * as request from "supertest";
 
-import { getApp } from './testing-module';
+import { getApp } from "./testing-module";
 
-describe('/ - Root routes', () => {
+describe("/ - Root routes", () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     app = await getApp();
   });
 
-  it('Home route', () => {
-    return request(app.getHttpServer()).get('/').expect(200).expect({ service_name: 'crud-manager-product-service' });
+  it("Home route", () => {
+    return request(app.getHttpServer()).get("/").expect(200).expect({
+      service_name: "crud-manager-product-service",
+      version: "1.0.0",
+    });
   });
 
-  it('Readyz route', () => {
-    return request(app.getHttpServer()).get('/readyz').expect(200).expect({
-      status: 'ok',
-      info: {},
-      error: {},
-      details: {},
-    });
+  it("Readyz route", () => {
+    return request(app.getHttpServer())
+      .get("/readyz")
+      .expect(200)
+      .expect({
+        status: "ok",
+        info: { mongodb: { status: "up" } },
+        error: {},
+        details: { mongodb: { status: "up" } },
+      });
   });
 });
